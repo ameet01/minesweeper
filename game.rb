83c999c92a537be7416e29ab36c53633ed7beae2
App Academy Student <student@appacademy.io>
2017-10-05 14:54:42 -0700

    Initial commit

---
+++ game.rb
@@ -0,0 +1,23 @@
+class Game
+
+  def initialize
+
+  end
+
+  def play
+    until over?
+
+    end
+  end
+
+  def over?
+
+  end
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  game = Game.new()
+  game.play
+end
